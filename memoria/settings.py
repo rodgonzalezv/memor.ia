35c5e12f032359cdd72312a88f3677655f7cb808
rodgonzalezv <rod.gonzalezv@duocuc.cl>
2024-07-04 01:10:03 -0400

    cambio en la ruta de las estaticao

--- memoria/settings.py
+++ memoria/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    #'/var/www/static',
+    '/home/memoria/memoria/static',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
