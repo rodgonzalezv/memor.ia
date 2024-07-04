04dde357577ad5a6d393608bd8b9e9747e988ba2
rodgonzalezv <rod.gonzalezv@duocuc.cl>
2024-07-03 23:58:51 -0400

    con plantillas

--- memoria/views.py
+++ memoria/views.py
@@ -1,5 +1,8 @@
-# mi_proyecto/views.py
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def hola_mundo(request):
     return HttpResponse("Hola Mundo")
+
+def simple(request):
+    return render(request, 'simple.html', {})
