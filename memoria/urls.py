f1156370a0f5ba82629023d817402d3523cce286
rodgonzalezv <rod.gonzalezv@duocuc.cl>
2024-07-04 00:24:55 -0400

    con paso de parametros

--- memoria/urls.py
+++ memoria/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import hola_mundo, simple
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', simple),
+    path('dinamico/<str:name>', views.dinamico, name='dinamico'),
 ]
--- memoria/views.py
+++ memoria/views.py
@@ -4,5 +4,10 @@ from django.shortcuts import render
 def hola_mundo(request):
     return HttpResponse("Hola Mundo")
 
-def simple(request):
-    return render(request, 'simple.html', {})
+def simple(request, name):
+    context = {'name':name}
+    return render(request, 'simple.html', context)
+
+def dinamico(request, name):
+    context={'name':name}
+    return render(request, 'simple.html', context)
