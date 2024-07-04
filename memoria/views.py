c651658b36fd09b100f76e52f6827f732946bba3
rodgonzalezv <rod.gonzalezv@duocuc.cl>
2024-07-04 02:03:08 -0400

    varias cargas

--- memoria/views.py
+++ memoria/views.py
@@ -1,14 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def hola_mundo(request):
-    return HttpResponse("Hola Mundo")
-
-def simple(request, name):
-    context = {'name':name}
-    return render(request, 'simple.html', context)
-
 def dinamico(request, name):
     arreglo=['josefina', 'matilda', 'javier','natalia']
     context={'name':name, 'nombres':arreglo}
-    return render(request, 'simple.html', context)
+    return render(request, 'web_inicio.html', context)
