d58442a6e23ac5c371cac2f3fe93d5f310791943
rodgonzalezv <rod.gonzalezv@duocuc.cl>
2024-07-04 12:12:17 -0400

    mas web

--- memoria/urls.py
+++ memoria/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
 #    path('admin/', admin.site.urls),
     path('', views.inicio, name='inicio'),
+    path('quienes_somos', views.quienes_somos, name='quienes_somos'),
+    path('planes', views.planes, name='planes'),
+    path('galeria', views.galeria, name='galeria'),
+    path('contacto', views.contacto, name='contacto'),
 ]
+
--- memoria/views.py
+++ memoria/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 def inicio(request):
     context={}
     return render(request, 'web_inicio.html', context)
+
+def quienes_somos(request):
+    context={}
+    return render(request, 'web_quienes_somos.html', context)
+
+def planes(request):
+    context={}
+    return render(request, 'web_planes.html', context)
+
+def galeria(request):
+    context={}
+    return render(request, 'web_galeria.html', context)
+
+def contacto(request):
+    context={}
+    return render(request, 'web_contacto.html', context)
