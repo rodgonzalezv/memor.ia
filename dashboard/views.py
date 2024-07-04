d778309d3f5a2122a58305cc19ba4ecbe025d150
rodgonzalezv <rod.gonzalezv@duocuc.cl>
2024-07-04 18:28:13 -0400

    rest

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def dashboard(request):
     context={}
     return render(request, 'dashboard_home.html', context)
+
+def registro(request):
+    context={}
+    return render(request, 'dashboard_user_registro.html', context)
