1eb05213fdd473568ce1a6741393a412e5064f78
rodgonzalezv <rod.gonzalezv@duocuc.cl>
2024-07-06 01:29:45 -0400

    staticfiles

--- core/settings.py
+++ core/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = '/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
